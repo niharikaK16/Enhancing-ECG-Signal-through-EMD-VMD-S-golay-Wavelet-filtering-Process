3acbeafd0dae6b46266bdc6be9c61912b29daf41
Niharika Kanapuram <164589511+niharikaK16@users.noreply.github.com>
2024-05-04 11:55:47 -0700

    Add files via upload

---
+++ EMDSgolay.m
@@ -0,0 +1,64 @@
+% Adding Gaussian noise to ECG signal
+function noisy_ecg = add_gaussian_noise(Original_signal, snr_db)
+signal_power = rms(Original_signal)^2;
+noise_power = signal_power / (10^(snr_db/10));
+noise = sqrt(noise_power) * randn(size(Original_signal));
+noisy_ecg = Original_signal + noise;
+end
+ 
+% Load ECG signal data
+load('/MATLAB Drive/100m.mat');
+ 
+val = (val - 0) / 200; 
+ 
+sig = val(1, 1:3600);
+ 
+Fs = 360; 
+ 
+t = (0:length(sig)-1) / Fs; 
+ 
+% Add Gaussian noise to the ECG signal
+snr_db = 10; 
+noisy_sig = add_gaussian_noise(sig, snr_db);
+ 
+% Plot original and noisy ECG signals
+figure;
+subplot(2,1,1);
+plot(t, sig, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (mV)');
+title('Original ECG Signal');
+grid on;
+ 
+subplot(2,1,2);
+plot(t, noisy_sig, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (mV)');
+title('Noisy ECG Signal');
+grid on;
+
+
+ 
+% Denoising ECG signal using Empirical Mode Decomposition (EMD) and Savitzky-Golay filter
+numIMFs = 4;
+spline_kind = 'pchip'; 
+ 
+[IMF, ~, ~] = emd(noisy_sig, 'Interpolation', spline_kind, 'Display', 0);
+ 
+IMF_to_filter = IMF(:, 1:numIMFs);
+ 
+% Apply Savitzky-Golay filtering to each IMF
+for i = 1:numIMFs
+   IMF_to_filter(:, i) = sgolayfilt(IMF_to_filter(:, i), 3, 11);
+end
+ 
+% Reconstruct denoised signal from filtered IMFs
+denoised_sig = sum(IMF_to_filter, 2);
+ 
+% Plot denoised ECG signal
+figure;
+plot(t, denoised_sig, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (mV)');
+title('Denoised ECG Signal (EMD + Savitzky-Golay)');
+grid on;
---
+++ VMDsGolayWavelet.m
@@ -0,0 +1,100 @@
+% Adding Gaussian noise to ECG signal
+function noisy_ecg = add_gaussian_noise(Original_signal, snr_db)
+signal_power = rms(Original_signal)^2;
+noise_power = signal_power / (10^(snr_db/10));
+noise = sqrt(noise_power) * randn(size(Original_signal));
+noisy_ecg = Original_signal + noise;
+end
+
+% Load ECG signal data
+load('/MATLAB Drive/100m.mat');
+ 
+val = (val - 0) / 200; 
+ 
+sig = val(1, 1:3600); 
+Fs = 360; 
+t = (0:length(sig)-1) / Fs; 
+
+snr_db = 10; 
+noisy_sig = add_gaussian_noise(sig, snr_db);
+ 
+% Plot original and noisy ECG signals
+figure;
+subplot(2,1,1);
+plot(t, sig, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (mV)');
+title('Original ECG Signal');
+grid on;
+ 
+subplot(2,1,2);
+plot(t, noisy_sig, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('ECG Amplitude (mV)');
+title('Noisy ECG Signal');
+grid on;
+
+% Variational Mode Decomposition (VMD)
+
+alpha = 2000; 
+tau = 0; 
+K = 4; 
+DC = 0; 
+init = 1; 
+tol = 1e-7; 
+
+[u, ~] = VMD(noisy_sig, alpha, tau, K, DC, init, tol); 
+
+% Transpose 'u' for easier processing if needed
+
+u = u'; 
+
+% Filter each mode using Savitzky-Golay filter
+
+order = 4; 
+frameLength = 11; 
+for i = 1:K
+
+if length(u(:, i)) >= frameLength 
+
+u(:, i) = sgolayfilt(u(:, i), order, frameLength);
+
+end
+
+end
+
+% Apply Discrete Wavelet Transform for further denoising each mode
+
+waveletName = 'db4'; 
+
+for i = 1:K
+
+[c, l] = wavedec(u(:, i), 5, waveletName); % 5
+
+sigma = median(abs(c)) / 0.6745; 
+
+thr = sigma * sqrt(2 * log(length(c))); 
+
+c = wthresh(c, 's', thr); 
+
+u(:, i) = waverec(c, l, waveletName); 
+
+end
+
+% Reconstruct the signal from the denoised modes
+
+denoised_sig = sum(u, 2); 
+
+% Plot denoised ECG signal
+
+figure;
+
+plot(t, denoised_sig, 'LineWidth', 2);
+
+xlabel('Time (s)');
+
+ylabel('ECG Amplitude (mV)');
+
+title('Denoised ECG Signal (VMD + S-Golay + Wavelet)');
+
+grid on;
